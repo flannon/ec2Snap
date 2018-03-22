710d8d00506b77ba505708e1db22872016567527
Flannon Jackson <flannon@nyu.edu>
2018-03-21 22:16:19 -0400

    Cleaned up some of the debugging commentary.

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/mitchellh/mapstructure"
 )
 
 // General refs
@@ -35,6 +36,7 @@ func service() *ec2.EC2 {
 // Get list of tagged instances
 // !+getTaggedInstances()
 func getTaggedInstances(t string) []*ec2.Instance {
+
 	params := &ec2.DescribeInstancesInput{
 		Filters: []*ec2.Filter{
 			{
@@ -78,8 +80,8 @@ func readBlockDeviceFromInstance(instance *ec2.Instance) (map[string]interface{}
 	for _, bd := range instance.BlockDeviceMappings {
 		if bd.Ebs != nil {
 			instanceBlockDevices[*bd.Ebs.VolumeId] = bd
-			fmt.Println("bd:", bd)
-			fmt.Println("Type of bd:", reflect.TypeOf(bd))
+			//fmt.Println("bd:", bd)
+			//fmt.Println("Type of bd:", reflect.TypeOf(bd))
 		}
 	}
 
@@ -98,60 +100,60 @@ func readBlockDeviceFromInstance(instance *ec2.Instance) (map[string]interface{}
 		VolumeIds: volIDs,
 	})
 	//fmt.Printf("volResp: %v\n", volResp)
-	fmt.Printf("Type of volResp: %v\n", reflect.TypeOf(volResp))
+	//fmt.Printf("Type of volResp: %v\n", reflect.TypeOf(volResp))
 	if err != nil {
 		return nil, err
 	}
 
 	for _, vol := range volResp.Volumes {
 		instanceBd := instanceBlockDevices[*vol.VolumeId]
-		fmt.Println("Type of instanceBd:", reflect.TypeOf(instanceBd))
+		//fmt.Println("Type of instanceBd:", reflect.TypeOf(instanceBd))
 		bd := make(map[string]interface{})
 
-		fmt.Println("#############")
+		//fmt.Println("#############")
 		// this is where the DescribeSnapshotVolume (bd) struct starts
-		bd["volume_id"] = *vol.VolumeId
-		fmt.Printf("bd[volume_id] %v\n", reflect.TypeOf(bd["volume_id"]))
+		bd["volumeId"] = *vol.VolumeId
+		//fmt.Printf("bd[volumeId] %v\n", reflect.TypeOf(bd["volumeId"]))
 
 		if instanceBd.Ebs != nil && instanceBd.Ebs.DeleteOnTermination != nil {
-			bd["delete_on_termination"] = *instanceBd.Ebs.DeleteOnTermination
+			bd["deleteOnTermination"] = *instanceBd.Ebs.DeleteOnTermination
 		}
-		fmt.Printf("bd[delete_on_termination] %v\n", reflect.TypeOf(bd["delete_on_termination"]))
+		//fmt.Printf("bd[deleteOnTermination] %v\n", reflect.TypeOf(bd["deleteOnTermination"]))
 		if vol.Size != nil {
-			bd["volume_size"] = *vol.Size
+			bd["volumeSize"] = *vol.Size
 		}
-		fmt.Printf("bd[volume_size] %v\n", reflect.TypeOf(bd["volume_size"]))
+		//fmt.Printf("bd[volumeSize] %v\n", reflect.TypeOf(bd["volumeSize"]))
 		if vol.VolumeType != nil {
-			bd["volume_type"] = *vol.VolumeType
+			bd["volumeType"] = *vol.VolumeType
 		}
-		fmt.Printf("bd[volume_type] %v\n", reflect.TypeOf(bd["volume_type"]))
+		//fmt.Printf("bd[volumeType] %v\n", reflect.TypeOf(bd["volumeType"]))
 		if vol.Iops != nil {
 			bd["iops"] = *vol.Iops
 		}
-		fmt.Printf("bd[iops] %v\n", reflect.TypeOf(bd["iops"]))
+		//fmt.Printf("bd[iops] %v\n", reflect.TypeOf(bd["iops"]))
 		if blockDeviceIsRoot(instanceBd, instance) {
 			blockDevices["root"] = bd
-			fmt.Printf("blockDevices[root] %v\n", reflect.TypeOf(blockDevices["root"]))
-			fmt.Println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+			//fmt.Printf("blockDevices[root] %v\n", reflect.TypeOf(blockDevices["root"]))
+			//fmt.Println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
 		} else {
 
 			if instanceBd.DeviceName != nil {
-				bd["device_name"] = *instanceBd.DeviceName
+				bd["deviceName"] = *instanceBd.DeviceName
 			}
-			fmt.Printf("bd[device_name] %v\n", reflect.TypeOf(bd["device_name"]))
+			//fmt.Printf("bd[deviceName] %v\n", reflect.TypeOf(bd["deviceName"]))
 			if vol.Encrypted != nil {
 				bd["encrypted"] = *vol.Encrypted
 			}
-			fmt.Printf("bd[encrypted] %v\n", reflect.TypeOf(bd["encrypted"]))
+			//fmt.Printf("bd[encrypted] %v\n", reflect.TypeOf(bd["encrypted"]))
 			if vol.SnapshotId != nil {
-				bd["snapshot_id"] = *vol.SnapshotId
+				bd["snapshotId"] = *vol.SnapshotId
 			}
-			fmt.Printf("bd[snapshot_id] %v\n", reflect.TypeOf(bd["snapshot_id"]))
-			fmt.Println("#############")
+			//fmt.Printf("bd[snapshotId] %v\n", reflect.TypeOf(bd["snapshotId"]))
+			//fmt.Println("#############")
 
 			blockDevices["ebs"] = append(blockDevices["ebs"].([]map[string]interface{}), bd)
-			fmt.Println("blockDevice[ebs]", blockDevices["ebs"])
-			fmt.Println("Type of blockDevices[ebs]", reflect.TypeOf(blockDevices["ebs"]))
+			//fmt.Println("blockDevice[ebs]", blockDevices["ebs"])
+			//fmt.Println("Type of blockDevices[ebs]", reflect.TypeOf(blockDevices["ebs"]))
 		}
 	}
 
@@ -167,41 +169,19 @@ func blockDeviceIsRoot(bd *ec2.InstanceBlockDeviceMapping, instance *ec2.Instanc
 } // !-blockDeviceIsRoot()
 
 // InstanceBlockDevice struct
-type InstanceBlockDevice struct {
-	BlockDeviceMappings []*ec2.BlockDeviceMapping
-}
-
-// InstanceSnapshotBlockDevice struct
-//type snapshotBlockDevice struct {
-//	SnapshotId          string
-//	VolumeId            string
-//	DeleteOnTermination bool
-//	VolumeSize          int64
-//	VolumeType          string
-//	Iops                int64
-//	DeviceName          string
-//	Encrypted           bool
+//type InstanceBlockDevice struct {
+//	BlockDeviceMappings []*ec2.BlockDeviceMapping
 //}
 
-//type SnapshotBlockDevice struct {
-//	volume_type           string
-//	iops                  int64
-//	device_name           string
-//	encrypted             bool
-//	snapshot_id           string
-//	volume_id             string
-//	delete_on_termination bool
-//	volume_size           int64
-//}
 type SnapshotBlockDevice struct {
-	Volume_size           int64
-	Volume_type           string
-	Iops                  int64
-	Device_name           string
-	Encrypted             bool
-	Snapshot_id           string
-	Volume_id             string
-	Delete_on_termination bool
+	VolumeId            string
+	DeleteOnTermination bool
+	VolumeSize          int64
+	VolumeType          string
+	Iops                int64
+	DeviceName          string
+	Encrypted           bool
+	SnapshotId          string
 }
 
 //type InstanceSnapshotBlockDevice struct {
@@ -209,82 +189,78 @@ type SnapshotBlockDevice struct {
 //	root                string
 //}
 
-type InstanceSnapshotBlockDevice struct {
-	SnapshotBlockDevice `mapstructure:",squash"`
-	Root                string
-}
+//type InstanceSnapshotBlockDevice struct {
+//	SnapshotBlockDevice `mapstructure:",squash"`
+//	Root                string
+//}
 
 //!+main
 func main() {
 
 	//fmt.Println("Testy")
-	svc := service()
-	_ = svc
-	// instances is a pointer to a slice of string
+	_ = service()
+	//_ = svc
 	tag := "Test"
 	instances := getTaggedInstances(tag)
-	//fmt.Println("Type of instances:", reflect.TypeOf(instances))
 
 	for _, i := range instances {
-		fmt.Println("Type of i:", reflect.TypeOf(i))
-		fmt.Println("i.InstanceId:", *i.InstanceId)
-		fmt.Println("-----------------------------------------------------------")
+		//fmt.Println("Type of i:", reflect.TypeOf(i))
+		//fmt.Println("i.InstanceId:", *i.InstanceId)
+		//fmt.Println("-----------------------------------------------------------")
 
 		//readBlockDeviceFromInstance(i)
 
 		//ibd, _ := readBlockDeviceFromInstance(i)
 		ibd, _ := readBlockDeviceFromInstance(i)
-		_ = ibd
+		//_ = ibd
 
-		/*
-			fmt.Println("-----------------------------------------------------------")
-			fmt.Printf("ibd: %v\n", ibd)
-			fmt.Println("-----------------------------------------------------------")
-			fmt.Printf("Type of ibd: %v\n", reflect.TypeOf(ibd))
+		//fmt.Println("-----------------------------------------------------------")
+		//fmt.Printf("ibd: %v\n", ibd)
+		//fmt.Println("-----------------------------------------------------------")
+		//fmt.Printf("Type of ibd: %v\n", reflect.TypeOf(ibd))
 
-			for k, v := range ibd {
-				// v is a slice of map[string]interface {}
-				// so first we have to split the slice
-				// then dig the map values out.
-				fmt.Println("============")
-				if k == "ebs" {
-					fmt.Printf("Key: %v, Value: %v\n", k, v)
-					fmt.Printf("Type of value: %v\n", reflect.TypeOf(v))
-					fmt.Printf("Type of value.kind: %v\n", reflect.TypeOf(v).Kind())
-					fmt.Printf("Value of value: %v\n", reflect.ValueOf(v))
+		for k, v := range ibd {
+			// v is a slice of map[string]interface {}
+			// so first we have to split the slice
+			// then dig the map values out.
+			fmt.Println("============")
+			if k == "ebs" {
+				//fmt.Printf("Key: %v, Value: %v\n", k, v)
+				//fmt.Printf("Type of value: %v\n", reflect.TypeOf(v))
+				//fmt.Printf("Type of value.kind: %v\n", reflect.TypeOf(v).Kind())
+				//fmt.Printf("Value of value: %v\n", reflect.ValueOf(v))
 
-					switch reflect.TypeOf(v).Kind() {
-					case reflect.Slice:
-						s := reflect.ValueOf(v)
+				switch reflect.TypeOf(v).Kind() {
+				case reflect.Slice:
+					s := reflect.ValueOf(v)
 
-						for i := 0; i < s.Len(); i++ {
-							fmt.Println(s.Index(i))
-							fmt.Println("s.Index.(i).Kind()", s.Index(i).Kind())
-							fmt.Println("s.Index.(i).Interface()", s.Index(i).Interface())
-							mi := s.Index(i).Interface()
-							fmt.Printf("mi: %+v\n", mi)
-							fmt.Printf("Type of mi: %+v\n", reflect.TypeOf(mi))
+					for i := 0; i < s.Len(); i++ {
+						//fmt.Println(s.Index(i))
+						//fmt.Println("s.Index.(i).Kind()", s.Index(i).Kind())
+						//fmt.Println("s.Index.(i).Interface()", s.Index(i).Interface())
+						mi := s.Index(i).Interface()
+						//fmt.Printf("mi: %+v\n", mi)
+						//fmt.Printf("Type of mi: %+v\n", reflect.TypeOf(mi))
 
-							//var result Ec2ebs
-							var result SnapshotBlockDevice
+						//var result Ec2ebs
+						var result SnapshotBlockDevice
 
-							if err := mapstructure.Decode(mi, &result); err != nil {
-								panic(err)
-							}
-							fmt.Printf("%+v\n", result)
-							fmt.Printf("result.Volume_size: %v\n", result.Volume_size)
-							fmt.Printf("result.Volume_type: %v\n", result.Volume_type)
-							fmt.Printf("result.Iops: %v\n", result.Iops)
-							fmt.Printf("result.Device_name: %v\n", result.Device_name)
-							fmt.Printf("result.Encrypted: %v\n", result.Encrypted)
-							fmt.Printf("result.Snapshot_id: %v\n", result.Snapshot_id)
-							fmt.Printf("result.Volume_id: %v\n", result.Volume_id)
-							fmt.Printf("result.Delete_on_termination: %v\n", result.Delete_on_termination)
+						if err := mapstructure.Decode(mi, &result); err != nil {
+							panic(err)
 						}
-
+						//fmt.Printf("%+v\n", result)
+						fmt.Printf("VolumeId: %v\n", result.VolumeId)
+						//fmt.Printf("result.DeleteOnTermination: %v\n", result.DeleteOnTermination)
+						//fmt.Printf("result.VolumeSize: %v\n", result.VolumeSize)
+						//fmt.Printf("result.VolumeType: %v\n", result.VolumeType)
+						//fmt.Printf("result.Iops: %v\n", result.Iops)
+						//fmt.Printf("result.DeviceName: %v\n", result.DeviceName)
+						//fmt.Printf("result.Encrypted: %v\n", result.Encrypted)
+						//fmt.Printf("result.SnapshotId: %v\n", result.SnapshotId)
 					}
+
 				}
 			}
-		*/
+		}
 	} //!-for
 } //!-main
